c065215180ea319d942c454b4b5b741c4cc67947
FatjonZogaj <fatjon.zogaj@hotmail.com>
2021-06-29 19:03:05 +0200

    Add Exp 4 plots; Rename plots

---
+++ core/experiments/exp4_impostor.py
@@ -0,0 +1,16 @@
+import sys
+
+import core.config as cfg
+
+cfg.POP_TYPES = {0: 0.25,
+                 1: 0.25,
+                 2: 0.25,
+                 3: 0.25}
+cfg.PLOT_RELATIVE = False
+cfg.PLOT_TOTAL = False
+cfg.GREEN_BEARD = True  # has to be true
+
+if len(sys.argv) > 2:
+    cfg.PLOT_RELATIVE = bool(sys.argv[1])
+if len(sys.argv) > 3:
+    cfg.PLOT_TOTAL = bool(sys.argv[2])
