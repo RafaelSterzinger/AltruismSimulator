768e43483813dbf772dd97a3c02843f139c5bb7a
Rafael <rafael.sterzinger@gmx.at>
2021-06-30 11:26:27 +0200

    Add additional experiment to method section

--- core/experiments/exp4_impostor.py
+++ core/experiments/exp4_impostor.py
@@ -4,8 +4,8 @@ import core.config as cfg
 
 cfg.POP_TYPES = {0: 0.25,
                  1: 0.25,
-                 2: 0.25,
-                 3: 0.25}
+                 2: 0.49,
+                 3: 0.01}
 cfg.PLOT_RELATIVE = True
 cfg.PLOT_TOTAL = False
 cfg.GREEN_BEARD = True  # has to be true
